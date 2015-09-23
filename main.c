7dee5b7ce19a7320ce5bfc4466e271d0427e573c
GZablot <zablot.guillaume@bbox.fr>
2015-09-23 20:15:52 +0200

    MaJ

--- main.c
+++ main.c
@@ -1,11 +1,22 @@
+#include <SDL/SDL.h>
+#ifndef DEF_SPRITE 
+#define DEF_SPRITE 
+#include "sprite.h"
+#endif
 #include <stdlib.h>
 #include <stdio.h>
-#include <SDL/SDL.h>
-
-void pause(void);
 
 int main(int argc, char *argv[])
 {
+  SDL_Surface *screen = NULL, *background = NULL, *ball = NULL;
+  SDL_Rect backgroundPosition, ballPosition;
+  int gameover = 0;
+
+  backgroundPosition.x = 0;
+  backgroundPosition.y = 0;
+  ballPosition.x = 365 ;
+  ballPosition.y = 150;
+
    SDL_Init(SDL_INIT_VIDEO);
 
    if (SDL_Init(SDL_INIT_VIDEO) == -1){
@@ -15,9 +26,8 @@ int main(int argc, char *argv[])
      exit(EXIT_FAILURE);
    }
 
-   SDL_Surface *screen = NULL;
 
-   screen = SDL_SetVideoMode(800, 600, 32, SDL_HWSURFACE);
+   screen = SDL_SetVideoMode(800, 600, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
 
    if (screen == NULL){
 
@@ -28,12 +38,22 @@ int main(int argc, char *argv[])
 
    SDL_WM_SetCaption("Football Game", NULL);
 
+   background = SDL_LoadBMP("background.bmp");
+   SDL_BlitSurface(background, NULL, screen, &backgroundPosition);
+
    //SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 17, 206, 112));
 
+   ball = SDL_LoadBMP("ball.bmp");
+   SDL_SetColorKey(ball, SDL_SRCCOLORKEY, SDL_MapRGB(ball->format, 255, 255, 255));
+   SDL_BlitSurface(ball, NULL, screen, &ballPosition);
+
    SDL_Flip(screen);
+   SDL_EnableKeyRepeat(10, 10);
 
-   pause();
+   update_events(gameover);
 
+   SDL_FreeSurface(background);
+   SDL_FreeSurface(ball);
    SDL_Quit();
 
    return EXIT_SUCCESS;
@@ -41,18 +61,4 @@ int main(int argc, char *argv[])
 
 
 
-void pause(void)
-{
-	int continuer = 1;
-	SDL_Event event;
 
- 	while (continuer)
-	{
-		SDL_WaitEvent(&event);
-		switch(event.type)
-		{
-			case SDL_QUIT:
-				continuer = 0;
-		}
-	}
-}
